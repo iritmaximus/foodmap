c16dbaac8063942ca3f72fcd301b21a301f18036
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2024-03-19 22:21:36 +0200

    added foods to seed

--- packages/api/src/sql/seed.sql
+++ packages/api/src/sql/seed.sql
@@ -7,3 +7,11 @@ INSERT INTO country (name) VALUES
     ("Japan"),
     ("Algeria"),
     ("Germany");
+
+
+INSERT INTO food (name, description, country_id) VALUES
+    ("Karjalan piirakka", "Sellane kiva jutska, tällee touhuut: tee tällasii juttui...", 1),
+    ("Lihapullat", "Ollaan vähän stereotyyppisiä nyt :D", 2),
+    ("Miso-keitto", "Sellanen kirkas keitto", 5),
+    ("Sushi", "Riisii ja kalaa käärittynä", 5),
+    ("Bratwurst", "Lihaa, ja paljon", 7);
