1fd867a258c754dd33e02703c458d058b53ce7ca
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2024-03-19 21:18:15 +0200

    added morgan to backend

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 import config from "./utils/config";
 import dbRouter from "./routes/dbRouter";
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("tiny"));
 
 
 // kill the app if database is not correctly opened
